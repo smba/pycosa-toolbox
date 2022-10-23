5ff06e0dca983cfc4dd10babb09fa1a9644d9994
autoblack <>
2022-10-23 17:48:08 +0000

    fixup: Format Python code with Black

--- pycosa/modeling.py
+++ pycosa/modeling.py
@@ -295,63 +295,60 @@ class CNFExpression:
 
             return ps, bitvec_constraints
 
+
 class VariabilityModel:
-    
     def __init__(self):
-        
+
         self.binary_options = dict()
-        
+
         self.dimacs = None
         self.binary_clauses = None
         self.bin_feature_to_index = None
         self.bin_index_to_feature = None
-    
+
     def from_parser(self, parser: Parser):
-        
+
         self.bin_feature_to_index = parser.get_features2index()
         self.bin_index_to_feature = parser.get_index2features()
-        
+
         self.dimacs = parser.get_clauses()
         print(self.dimacs)
         clauses = parser.get_clauses()
-        
+
         z3_clauses = self._generate_z3_clauses(clauses)
-        
+
         self.binary_clauses = z3_clauses
         self.binary_options = parser.get_features()
-                
-    def _generate_z3_clauses(self, clauses):    
+
+    def _generate_z3_clauses(self, clauses):
         z3_clauses = []
         for clause in clauses:
             ors = []
             for literal in clause:
-                sign = (literal > 0)
+                sign = literal > 0
                 index = abs(literal)
                 feature_name = self.bin_index_to_feature[index]
-                
+
                 if sign:
                     add = z3.Bool(feature_name)
                 else:
                     add = z3.Not(z3.Bool(feature_name))
-                
+
                 ors.append(add)
-                
+
             if len(ors) > 1:
-                z3_clauses.append(
-                    z3.Or(ors)    
-                )
+                z3_clauses.append(z3.Or(ors))
             else:
                 z3_clauses.append(ors[0])
-                
+
         return z3_clauses
-    
-    
+
     def get_binary_clauses(self):
         return self.binary_clauses
-    
+
     def get_binary_features(self):
         return self.binary_options
-    
+
     def shuffle(self):
         clauses = []
         for clause in self.dimacs:
@@ -361,10 +358,10 @@ class VariabilityModel:
 
         idx = np.random.choice(len(clauses), replace=False, size=len(clauses))
         new_clauses = [clauses[i] for i in idx]
-        
+
         self.dimacs = new_clauses
         self.binary_clauses = self._generate_z3_clauses(new_clauses)
-    
+
+
 if __name__ == "__main__":
     pass
-    
