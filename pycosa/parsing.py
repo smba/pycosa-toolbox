83d95fea54fe5fa97043edbc324810e8b4390206
Stefan Mühlbauer <muehlbauer@informatik.uni-leipzig.de>
2022-10-23 19:39:37 +0200

    added variability model based on boolean literals

--- pycosa/parsing.py
+++ pycosa/parsing.py
@@ -35,14 +35,21 @@ class Parser:
         if feature_name in self._feature_to_index:
             return self._feature_to_index[feature_name]
         else:
-            msg = "No feature index for feature name '{}' found!".format(feat)
+            msg = "No feature index for feature name '{}' found!".format(feature_name)
             raise ValueError(msg)
 
     def get_clauses(self) -> Sequence[Sequence[int]]:
-        return self.clauses
+        return self._clauses
 
     def get_features(self) -> Sequence[str]:
         return list(self._feature_to_index.keys())
+    
+    def get_features2index(self):
+        return self._feature_to_index
+
+    def get_index2features(self):
+        return self._index_to_feature
+
 
     def parse(self, path: str) -> None:
         raise NotImplementedError()
@@ -71,20 +78,25 @@ class DimacsParser(Parser):
 
             if start == 'c':  # c = comment, used for specifying a feature
                 line = line.split(' ')
-                index = line[1]
+                index = int(line[1])
                 feature_name = line[2]
+                
+                # record
+                self._index_to_feature[index] = feature_name
+                self._feature_to_index[feature_name] = index
+                
             elif start == 'p':  # p = .. something, used for validation
+                
                 line = line.split(' ')
                 n_options = int(line[2])
                 n_clauses = int(line[3])
 
                 assert len(self._index_to_feature) == len(
-                    self._feature_to_index)
+                   self._feature_to_index)
                 assert len(self._index_to_feature) == n_options
 
             else:  # any other line should specify a clause
                 line = line.split(' ')
-
                 assert int(line[-1]) == 0
 
                 clause = [int(literal) for literal in line[:-1]]
@@ -97,9 +109,6 @@ class FeatureIdeParser(Parser):
         raise NotImplementedError()
 
     def parse(self, path: str) -> None:
-        raise NotImplementedError()
-
-    def to_cnf(self) -> CNFExpression:
         raise NotImplementedError()
 
 
@@ -115,10 +124,6 @@ class SPLCParser(Parser):
         raise NotImplementedError()
         
 
-    def to_cnf(self) -> CNFExpression:
-        raise NotImplementedError()
-
-
 class SPLOTParser(Parser):
 
     def __init__(self, ):
@@ -127,8 +132,6 @@ class SPLOTParser(Parser):
     def parse(self, path: str) -> None:
         raise NotImplementedError()
 
-    def to_cnf(self) -> CNFExpression:
-        raise NotImplementedError()
 
 
 if __name__ == "__main__":
