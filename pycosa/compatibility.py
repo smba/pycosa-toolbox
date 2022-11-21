95a91cabbee317cb7124ced1a9b3252392226c56
Stefan Mühlbauer <smba@users.noreply.github.com>
2022-11-21 17:38:56 +0100

    Update compatibility.py

--- pycosa/compatibility.py
+++ pycosa/compatibility.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 
-def read_measurements(path: str):
+def read_measurements(path, METRIC):
 
     root = ET.parse(path).getroot()
 
@@ -14,29 +14,35 @@ def read_measurements(path: str):
         for data in row:
             col = data.attrib["columname"]
             val = data.text.strip()
-            if col == "Performance":
+            if col == METRIC:
                 perf = float(val)
                 performance.append(perf)
 
             elif col == "Configuration":
                 options = val.split(",")
+                options = list(filter(lambda x: len(x) > 0, options))
                 for option in options:
                     features.add(option)
                 configs.append(options)
-
+    
+    res = []
+    performance = np.array(performance)
     # make data frame
     features = sorted(list(features))
-    data = pd.DataFrame(
-        np.zeros(shape=(len(performance), len(features) + 1)),
-        columns=features + ["performance"],
-    )
-
     for i, config in enumerate(configs):
-        data.loc[i, config] = 1
+        encoded = [1 if f in config else 0 for f in features]
+        encoded += [performance[i]]
+        
+        res.append(encoded)
 
-    for feature in features:
-        data[feature] = data[feature].astype("bool")
-
-    data["performance"] = performance
-
-    return data
+    res = np.vstack(res)
+    df = pd.DataFrame(
+        res,
+        columns = features + [METRIC],
+        index = np.arange(len(configs))
+    )
+    
+    for f in features:
+        df[f] = df[f].astype('bool')
+    
+    return df
