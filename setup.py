e96bfcf6accff3a2a7f9adf19c0e633de8b3da07
Stefan Mühlbauer <smba@users.noreply.github.com>
2022-11-21 18:02:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,6 +19,12 @@ setup_args = dict(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.8",
+    
+    # ehem
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True
+    
 )
 
 with open("requirements.txt") as f:
